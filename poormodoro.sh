14c40a2cfa8ad53524ebf13399aeb5a424b099bc
Emmanuel Dupont <emdupp@gmail.com>
2015-05-29 23:06:58 +0200

    Added error message if the script is not ran using sudo

--- poormodoro.sh
+++ poormodoro.sh
@@ -30,12 +30,18 @@ function pomodoro {
   while [ $sequence_number -lt $sequence_total ]; do
       echo "blacklist starts for 60 secs"
       black_list
+      echo "start working"
       sleep 30
       echo "blacklist cleared for 15 secs"
       clear_black_list
+      echo "pause starts"
       sleep 15
       $sequence_number=$sequence_number+1
   done
 }
-
-pomodoro
+if [[ $(id -u) -ne 0 ]]; then
+  echo "Use sudo to launch poormodoro!"
+  exit 1
+else
+  pomodoro
+fi
